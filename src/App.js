9996a04262a8ca04a94d7ee907dfb53c91401ea4
Burak İskender <burak.iskendr@gmail.com>
2022-11-28 14:51:53 +0300

    :recycle: refactor

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import { useSelector } from 'react-redux'
 import AddTodo from './components/AddTodo'
 import Modal from './components/Modal'
 import TodoList from './components/TodoList'
-import {} from './context/modalSlice'
 
 const App = () => {
-    const {open: isModalOpen } = useSelector(state => state.modal)
+    const { open: isModalOpen } = useSelector(state => state.modal)
     return (
         <div className='app'>
-            {isModalOpen && <Modal/>}
+            {isModalOpen && <Modal />}
             <h1>Todo App</h1>
             <AddTodo />
             <TodoList />
